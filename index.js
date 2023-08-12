58d9f3f81a035f5c362a35c50e165bfd0fd867e7
Mohammed Khan <60229505+saifkhan7865@users.noreply.github.com>
2023-08-12 21:04:29 +0530

    initial set up for youtube video for baileys

---
+++ index.js
@@ -0,0 +1,45 @@
+const {
+  DisconnectReason,
+  useMultiFileAuthState,
+} = require("@whiskeysockets/baileys");
+
+const makeWASocket = require("@whiskeysockets/baileys").default;
+
+async function connectionLogic() {
+  const { state, saveCreds } = await useMultiFileAuthState("auth_info_baileys");
+  const sock = makeWASocket({
+    // can provide additional config here
+    printQRInTerminal: true,
+    auth: state,
+  });
+
+  sock.ev.on("connection.update", async (update) => {
+    const { connection, lastDisconnect, qr } = update || {};
+
+    if (qr) {
+      console.log(qr);
+      // write custom logic over here
+    }
+
+    if (connection === "close") {
+      const shouldReconnect =
+        lastDisconnect?.error?.output?.statusCode !==
+        DisconnectReason.loggedOut;
+
+      if (shouldReconnect) {
+        connectionLogic();
+      }
+    }
+  });
+
+  sock.ev.on("messages.update", (messageInfo) => {
+    console.log(messageInfo);
+  });
+
+  sock.ev.on("messages.upsert", (messageInfoUpsert) => {
+    console.log(messageInfoUpsert);
+  });
+  sock.ev.on("creds.update", saveCreds);
+}
+
+connectionLogic();
